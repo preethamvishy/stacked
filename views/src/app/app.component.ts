d30a67a8bfd4eac0b656d468dffa050ce9e8af6e
Preetham Viswanathan <preethamvishy10@gmail.com>
2019-01-31 05:31:38 -0500

    bootstrap, font awesome added

--- views/src/app/app.component.ts
+++ views/src/app/app.component.ts
@@ -2,8 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: ``,
-  styles: []
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'app';
--- views/src/app/app.module.ts
+++ views/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { AuthComponent } from './auth/auth.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AuthComponent,
   ],
   imports: [
     BrowserModule
