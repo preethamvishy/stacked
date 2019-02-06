4b6ce25e78e1b45a07558d541bd8b6be896860a7
Preetham Viswanathan <preethamvishy10@gmail.com>
2019-02-06 04:48:13 -0500

    tweet composed revamped. drag-drop or select up to 4 images.

--- views/src/app/dashboard/dashboard.component.ts
+++ views/src/app/dashboard/dashboard.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { DomSanitizer, SafeResourceUrl, SafeUrl } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-dashboard',
@@ -10,20 +11,51 @@ export class DashboardComponent implements OnInit {
   content: any = {
     text: ""
   }
-  constructor() { }
+  fileUrls = [];
+
+  constructor(private sanitizer: DomSanitizer) { }
 
   ngOnInit() {
   }
 
-  input(e)
-  {
+  input(e) {
     var ele = document.getElementById("textarea");
-    if(e.length > 35) {
+    if (e.length > 35) {
       ele.style.transition = '0.4s';
       ele.style.fontSize = '20px';
     }
-    else 
+    else
       ele.style.fontSize = '32px';
   }
 
+  dropContainerChange(event) {
+
+    let fileList = event.target.files;
+    for (var file of fileList) {
+      if (this.fileUrls.length == 4)
+        break;
+      if (!this.isImage(file))
+        continue;
+      this.fileUrls.push(this.sanitizer.bypassSecurityTrustResourceUrl
+        (URL.createObjectURL(file)));
+    }
+  }
+  dragOver() {
+    document.getElementById('imageDropContainer').className = 'dragHover imageDropContainer';
+  }
+
+  dropImage() {
+    document.getElementById('imageDropContainer').className = 'imageDropContainer';
+  }
+
+  isImage(filename) {
+    if (filename.type.split('/')[0].toLowerCase() == 'image') {
+      return true;
+    }
+    return false;
+  }
+
+  dragleave() {
+    document.getElementById('imageDropContainer').className = 'dragHoverimageDropContainer';
+  }
 }
